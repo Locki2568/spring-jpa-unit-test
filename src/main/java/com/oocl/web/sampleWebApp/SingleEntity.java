02a416f22b40d3641d39430a55416e7f449994be
Locki Lo <locki.lo@cargosmart.com>
2018-11-30 11:51:57 +0800

    [Locki] test: when entity with long name should throws exception

--- src/main/java/com/oocl/web/sampleWebApp/SingleEntity.java
+++ src/main/java/com/oocl/web/sampleWebApp/SingleEntity.java
@@ -1,15 +1,12 @@
 package com.oocl.web.sampleWebApp;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class SingleEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     public Long id;
+    @Column(length = 10)
     public String name;
 
     public SingleEntity(){
--- src/test/java/com/oocl/web/sampleWebApp/SingleEntityTest.java
+++ src/test/java/com/oocl/web/sampleWebApp/SingleEntityTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.persistence.EntityManager;
+
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
@@ -16,16 +18,35 @@ public class SingleEntityTest {
     @Autowired
     private SingleEntityRepository singleEntityRepository;
 
+    @Autowired
+    private EntityManager entityManager;
+
+    @Test
+    public void should_throws_exception_when_name_lenth_too_long(){
+        //Given A single entity obj with name too long
+        SingleEntity singleEntityWithLongName =new SingleEntity();
+        singleEntityWithLongName.name = "NameWithTooManyCharacter";
+        singleEntityWithLongName.id = 1L;
+        //When save into repository should throw exception
+        AssertHelper.assertThrows(Exception.class, () ->{
+            singleEntityRepository.save(singleEntityWithLongName);
+            entityManager.flush();
+        });
+    }
+
     @Test
     public void should_test_single_entity(){
         //Given a single entity obj and saved into repository
         SingleEntity singleEntity = new SingleEntity();
         singleEntity.name = "HI";
-        singleEntity.id = 1L;
+        singleEntity.id = 2L;
         singleEntityRepository.save(singleEntity);
+        entityManager.flush();
 
         // When get back single entity from repository
-        SingleEntity singleEntityFromRepo = singleEntityRepository.getOne(1L);
+        entityManager.clear();
+        SingleEntity singleEntityFromRepo = singleEntityRepository.getOne(2L);
+
 
         // Then assert equal should return true
         assertEquals("HI", singleEntityFromRepo.name);
